2b3e4120dd5eb059efe48a667d21fd468994bc56
cmrd-a <cmrd.a@ya.ru>
2022-09-04 15:25:28 +0300

    Добавлена проверка прав.

--- auth/src/Auth/commands.py
+++ auth/src/Auth/commands.py
@@ -7,18 +7,20 @@ from Auth.extensions import db
 @click.command()
 @click.argument("email")
 @click.argument("password")
-def create_super_user(email, password):
-    base_permissions = ["comment", "manage_users", "watch_new_movies"]
-    base_roles = {"registered": ["comment"], "super_user": base_permissions}
+def create_superuser(email, password):
+    base_roles = {
+        "registered": ["comment"],
+        "superuser": ["comment", "manage_users", "watch_new_movies"],
+    }
     for role_name, permissions in base_roles.items():
         permissions_objects = []
-        for permission_name in base_permissions:
+        for permission_name in permissions:
             permission = Permission.query.filter_by(name=permission_name).first()
             if not permission:
                 permission = Permission(name=permission_name)
                 db.session.add(permission)
                 db.session.commit()
-                permissions_objects.append(permission)
+            permissions_objects.append(permission)
         role = Role.query.filter_by(name=role_name).first()
         if not role:
             role = Role(name=role_name, permissions=permissions_objects)
@@ -27,7 +29,7 @@ def create_super_user(email, password):
 
     user = User.query.filter_by(email="email").first()
     if not user:
-        super_role = Role.query.filter_by(name="super_user").first()
+        super_role = Role.query.filter_by(name="superuser").first()
         user = User(email=email, password=password, role=super_role)
         db.session.add(user)
         db.session.commit()
